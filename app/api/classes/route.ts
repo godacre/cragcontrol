f7d77079cedb0b5d8882992314102ada4a20346f
Matty <154713846+godacre@users.noreply.github.com>
2026-03-26 20:38:38 -0400

    Delete app/api/classes/route.ts

--- app/api/classes/route.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-import { NextResponse } from 'next/server';
-
-const prisma = new PrismaClient();
-
-export async function GET() {
-  const classes = await prisma.class.findMany();
-  return NextResponse.json(classes);
-}
